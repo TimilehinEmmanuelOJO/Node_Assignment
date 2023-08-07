80c43ef5ae22fbdfbf54aef74b9be13b875bc3ee
Timilehin Emmanuel OJO <timilehin175@gmail.com>
2023-08-07 21:48:47 +0100

    Update Fs_mod.js
    Adjustments

--- Fs_mod.js
+++ Fs_mod.js
@@ -1,128 +1,105 @@
 const fs = require('fs')
 const path = require('path')
 
-// Using the FS module:
-// Create directory/folder named: “Students”
 
-// Path to the directory you want to create
-const directoryPath = '\Students';
-const fileName = 'user.js';
-const filePath = path.join(directoryPath, fileName);
-const oldDirName = "Students"
-const newDirName = "Names"
-const myName = 'Emmanuel Timilehin Ojo'
-const userInfo = {
-    name: 'Timi Ojo',
-    age: 30,
-    sex: 'Male',
-    nationality: 'Nigerian',
-    phoneNumber: '08163393738'
-};
+//Create folder
 
-const oldFileName = 'user.js';
-const newFileName = 'timi_ojo.js';
+ const newDirPath = path.join(__dirname, "Students") 
+ console.log(newDirPath)
 
-const oldFilePath = path.join(newDirName, oldFileName);
-const newFilePath = path.join(newDirName, newFileName);
+ fs.mkdir(newDirPath, (err) => {
+     if(err){
+         console.log(err)
+     }else{
+         console.log('folder created successfully')
+     }
+ })
 
 
+ //Update the student folder to Names
+ const newFold = path.join(__dirname, "Names")
+
+ fs.rename(newDirPath, newFold, (err) => {
+    if(err){
+        console.log(err)
+    return
+    }else{console.log('file changed Successfully')}
+ }
+ ) 
+
+// //In the Students directory, create a file named user.js
+
+ const newFilePath = path.join(__dirname, 'Students', 'Users.js')
+
+ console.log(newFilePath)
+ const content = "This is another content"
+
+ fs.writeFile(newFilePath, content,(err) => {
+      if(err){
+          console.log(`this is the error: ${err}`)
+          return
+      }else{
+          console.log("File written")
+      } 
+  })
+
+//Add my name
+
+ const newFilePath2 = path.join(__dirname, 'Students', 'Users.js')
+ const nameC = "Emmanuel Timilehin Ojo"
+
+ fs.writeFile(newFilePath2, nameC, (err) => {
+     if(err){
+               console.log(`this is the error: ${err}`)
+                 return
+               }else{
+                   console.log("File written")
+               } 
+ })
+
+//Apend other details to file
+
+ const details = '\nAge: 30\n Male\n Nigerian\n 08163393738\n I am a cool guy'
+ const newFilePath3 = path.join(__dirname, 'Students', 'Users.js')
+
+ fs.appendFile(newFilePath3, details, (err) => {
+     if(err){
+         console.log(err)
+     }else{
+         console.log('File Updated Successfully')
+     }
+ })
 
 
+//rename file
+const newFileName = path.join(__dirname, 'Emmanuel_ojo.js')
 
- if(!fs.existsSync(directoryPath)){
-     fs.mkdirSync(directoryPath)
-     console.log("folder created successfully")
- }else{console.log('Directory already exists.')}
-
-
-
-// // In the Students directory, create a file named user.js
-if (!fs.existsSync(filePath)){
-    fs.writeFileSync(filePath, '', 'utf-8',)
-    console.log('File created successfully.');
-}else{
-    console.log('File already exists.');
-}
-
-
-// // Update the Students directory to “Names”
-//  
-
-  if (fs.existsSync(oldDirName)){
-      fs.renameSync(oldDirName, newDirName)
-          console.log("Successfully renamed to Names")
-  }else{
-      console.log('Dir not found')
-  }
-
- // Add your name as content to the file user.js
-  
- if (!fs.existsSync(newDirName)) {
-    console.log('Directory not found.');
-} else {
-    // Check if the file exists
-    if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath, myName, 'utf-8');
-        console.log('File created and content added successfully.');
-    } else {
-        fs.writeFileSync(filePath, myName, 'utf-8');
-        console.log('Content added to existing file successfully.');
+fs.rename(newFilePath, newFileName, (err) => {
+    if(err){
+        console.log(err)
+        return
+    }else{
+        console.log('File renamed successfully')
     }
-}
+})
 
-
-// // Update the file and add your age, sex, nationality, phone number and any other information about yourself
-
-
-// Check if the directory exists
-if (!fs.existsSync(newDirName)) {
-    console.log('Directory not found.');
-} else {
-    // Write the user information to the file
-    fs.writeFileSync(filePath, userInfo, 'utf-8');
-    console.log('User info added successfully.');
-}
-
-
-
-// // Update the file user.js to {your_name}.js eg daniel_adesoji.js
-
-
-
-// Check if the directory exists
-if (!fs.existsSync(directoryPath)) {
-    console.log('Directory not found.');
-} else {
-    // Check if the old file exists
-    if (fs.existsSync(oldFilePath)) {
-        // Rename the file
-        fs.renameSync(oldFilePath, newFilePath);
-        console.log('File renamed successfully.');
-    } else {
-        console.log('File not found.');
+//read file
+fs.readFile(newFilePath, "utf8", (err, data) => {
+    if (err){
+        console.log(`This is the error that was thrown: ${err}`)
+        return
+    }else{
+        console.log(data)
     }
-}
+})
 
+//Remove file
+const newFile = path.join(__dirname, 'files', 'Emmanuel_ojo.js')
 
-// // Read the contents from {your_name}.js. User fs.open or fs.readFile
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-    } else {
-        console.log('File content:');
-        console.log(data);
+fs.rm(newFile, (err) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log("file removed successfully")
     }
-});
-
-
-// // Delete the file {your_name}.js
-fs.unlink(filePath, (err) => {
-    if (err) {
-        console.error('Error deleting the file:', err);
-    } else {
-        console.log('File deleted successfully.');
-    }
-});
-
-// // Delete the directory “Names”
-
+})
